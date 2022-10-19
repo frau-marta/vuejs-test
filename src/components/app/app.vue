7659bd43a63d83ea30abdfd4294e32dbe3119bf6
Elena Nikolaeva <elena.nikolaeva@convergent.digital>
2022-10-19 18:02:53 +0300

    Add files

---
+++ src/components/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app">
+    <h3>Привет, мир!</h3>
+  </div>
+</template>
+<style lang="scss" src="./app.scss"></style>
+<script>
+	export default {
+        name: 'App',
+    }
+</script>
